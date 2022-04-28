bf9bcbbc202032ca6e608952d9a7fce3381106a4
SouravDhawa <100849954+SouravDhawa@users.noreply.github.com>
2022-04-28 23:09:54 +0530

    Add files via upload

---
+++ newreact/component/navbar.js
@@ -0,0 +1,13 @@
+function navbar(){
+    return `
+    <h3><a href="./index.html">Home</a></h3>
+    <div id="option">
+        <h3><a href="./jewelery.html">jewelery</a></h3>
+        <h3><a href="./electronics.html">electronics</a></h3>
+        <h3><a href="login.html">login</a></h3>
+        <h3><a href="signup.html">sign in</a></h3>
+    </div>
+    `
+}
+
+export {navbar};
---
+++ newreact/src/index.js
@@ -0,0 +1,7 @@
+import { navbar } from "../component/navbar.js";
+
+import '../style/navbar.css'
+document.getElementById("navbar").innerHTML=navbar();
+
+
+
